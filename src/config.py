fc975c74d00573e2a7d6e60b74c5d646d2b57050
Sarthak Kumar Singh <sks19june@gmail.com>
2025-05-10 21:59:06 +0530

    minor changes

--- src/config.py
+++ src/config.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# HUGGING_FACE_MODEL = st.secrets["HUGGING_FACE_MODEL"]
-# LLAMA_MODEL = st.secrets["LLAMA_MODEL"]
-# PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-# PINECONE_INDEX = st.secrets["PINECONE_INDEX"]
-# GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+# HUGGING_FACE_MODEL = os.getenv("HUGGING_FACE_MODEL")
+# LLAMA_MODEL = os.getenv("LLAMA_MODEL")
+# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+# PINECONE_INDEX = os.getenv("PINECONE_INDEX")
+# GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 HUGGING_FACE_MODEL = st.secrets["api_keys"]["HUGGING_FACE_MODEL"]
 LLAMA_MODEL = st.secrets["api_keys"]["LLAMA_MODEL"]
