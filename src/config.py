d4e4abd59db3280a300a2a76dcac391f33b49bc2
Sarthak Kumar Singh <sks19june@gmail.com>
2025-05-10 20:11:49 +0530

    deployment changes

--- src/config.py
+++ src/config.py
@@ -12,11 +12,11 @@ load_dotenv()
 # PINECONE_INDEX = st.secrets["PINECONE_INDEX"]
 # GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
-HUGGING_FACE_MODEL = os.getenv("HUGGING_FACE_MODEL")
-LLAMA_MODEL = os.getenv("LLAMA_MODEL")
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_INDEX = os.getenv("PINECONE_INDEX")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HUGGING_FACE_MODEL = st.secrets["api_keys"]["HUGGING_FACE_MODEL"]
+LLAMA_MODEL = st.secrets["api_keys"]["LLAMA_MODEL"]
+PINECONE_API_KEY = st.secrets["api_keys"]["PINECONE_API_KEY"]
+PINECONE_INDEX = st.secrets["api_keys"]["PINECONE_INDEX"]
+GROQ_API_KEY = st.secrets["api_keys"]["GROQ_API_KEY"]
 
 
 pinecone_client = Pinecone(api_key=PINECONE_API_KEY)
